ff6931b83f690f12820e9b84fdbc0cb5127d2ac4
jairo8925 <csarmiento2333@gmail.com>
2021-03-02 19:10:34 -0800

    Added user input

---
+++ Music Advisor/task/src/advisor/Main.java
@@ -0,0 +1,64 @@
+package advisor;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            String[] choice = console.nextLine().split(" ");
+            switch (choice[0]) {
+                case "featured":
+                    displayFeatured();
+                    break;
+                case "new":
+                    displayNew();
+                    break;
+                case "categories":
+                    displayCategories();
+                    break;
+                case "playlists":
+                    displayPlaylist(choice[1]);
+                    break;
+                case "exit":
+                    running = false;
+                    System.out.println("---GOODBYE!---");
+                    break;
+            }
+        }
+    }
+
+    public static void displayFeatured() {
+        System.out.println("---FEATURED---");
+        System.out.println("Mellow Morning");
+        System.out.println("Wake Up and Smell the Coffee");
+        System.out.println("Monday Motivation");
+        System.out.println("Songs to Sing in the Shower");
+    }
+
+    public static void displayNew() {
+        System.out.println("---NEW RELEASES---");
+        System.out.println("Mountains [Sia, Diplo, Labrinth]");
+        System.out.println("Runaway [Lil Peep]");
+        System.out.println("The Greatest Show [Panic! At The Disco]");
+        System.out.println("All Out Life [Slipknot]");
+    }
+
+    public static void displayCategories() {
+        System.out.println("---CATEGORIES---");
+        System.out.println("Top Lists");
+        System.out.println("Pop");
+        System.out.println("Mood");
+        System.out.println("Latin");
+    }
+
+    public static void displayPlaylist(String name) {
+        System.out.println("---" + name.toUpperCase() + " PLAYLISTS---");
+        System.out.println("Walk Like A Badass");
+        System.out.println("Rage Beats");
+        System.out.println("Arab Mood Booster");
+        System.out.println("Sunday Stroll");
+    }
+}
